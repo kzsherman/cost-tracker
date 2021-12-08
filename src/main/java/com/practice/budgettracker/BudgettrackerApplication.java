063ef1749edbc68a047cd1b981b560f178e51d73
Roman <romanshe@gmail.com>
2021-12-09 02:33:10 +0600

    initial commit includes dependencies: Web, JPA, Thymeleaf, MySQL, SpringSecurity

---
+++ src/main/java/com/practice/budgettracker/BudgettrackerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.budgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgettrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgettrackerApplication.class, args);
+    }
+
+}
